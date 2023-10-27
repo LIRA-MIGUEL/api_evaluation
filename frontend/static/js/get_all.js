d292ebf2efaabca88cae9b314a396ec58faa4ead
LIRA-MIGUEL Utec <1722110271@utectulancingo.edu.mx>
2023-10-27 15:01:00 +0000

    commit

--- frontend/static/js/get_all.js
+++ frontend/static/js/get_all.js
@@ -1,12 +1,33 @@
 function getAll(){
     var request = new XMLHttpRequest;
-    request.open('GET', "http://127.0.0.1:8000/contactos");
+    request.open('GET', "https://8000-liramiguel-apievaluatio-7q8jjo4hymr.ws-us105.gitpod.io");
     request.send();
 
     request.onload = (e) => {
         const response = request.responseText;
         const json = JSON.parse(response);
-        console.log("response" + json.response);
+        console.log("response" + response);
+        console.log("json: " + json);
+        console.log ("status_code" + request_status);
 
+        console.log("Email: " + json[0]["email"]);
+        console.log("Nombre: " + json[0]["nombre"]);
+        console.log("Telefono:" + json[0]["telefono"]);
+
+        const tbody_contactos = document.getElementById("tbody_contactos");
+        var tr = document = createElement("tr");
+        var td_email = document.createElement("td");
+        var td_nombre = document.createElement("td");
+        var td_telefono = document.createElement("td");
+
+        td_email.innerHTML = json[0]["email"];
+        td_nombre.innerHTML = json[0]["nombre"];
+        td_telefono.innerHTML = json[0]["telefono"];
+
+        tr.appendChild(td_email);
+        tr.appendChild(td_nombre);
+        tr.appendChild(td_telefono);
+
+        tbody_contactos.appendChild(tr);
     };
 };

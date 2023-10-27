63ad3d6765db140cdf1762bf184ecb6287f6115f
LIRA-MIGUEL <1722110271@utectulancingo.edu.mx>
2023-10-27 07:50:20 -0600

    se actualizo el repo heroku_mysql

--- contactos.sql
+++ contactos.sql
@@ -1,11 +1,11 @@
-CREATE TABLE contactos (
+-- Crear una tabla de contactos
+CREATE TABLE IF NOT EXISTS contactos (
     email TEXT PRIMARY KEY,
     nombre TEXT,
     telefono TEXT
 );
 
-INSERT INTO contactos (email, nombre, telefonos)
-VALUES ("juan@example.com", "Juan Pérez", "555-123-4567");
-
-INSERT INTO contactos (emial, nombre, telefono)
-VALUES ("maria@example.com", "María García", "555-678-9012");
+-- Insertar algunos datos de ejemplo
+INSERT INTO contactos (email, nombre, telefono) VALUES
+    ('juan@example.com', 'Juan Pérez', '555-1234'),
+    ('maria@example.com', 'María López', '555-5678');
